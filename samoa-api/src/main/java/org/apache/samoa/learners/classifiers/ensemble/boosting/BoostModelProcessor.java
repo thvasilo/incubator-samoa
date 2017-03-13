18a9b3cdbe5d32dc77b2307df804e3a70b7dcf0d
Theodore Vasiloudis <tvas@sics.se>
2017-03-13 12:42:20 +0100

    Blocking sync for Boosting, using an in-memory buffer.
    In this design we buffer the elements sent in from the source until the preceding
    elements have been fully processed and output from the boosting pipeline.
    
    If the last event is received at the input but there are still events in the buffer,
    we just flush them down the pipeline. The accumulation of events depends on the
    speed of the learning algorithm: If we are able to get through the pipeline fast,
    not many events should accumulate in the buffer.

--- samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/boosting/BoostModelProcessor.java
+++ samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/boosting/BoostModelProcessor.java
@@ -19,6 +19,7 @@ package org.apache.samoa.learners.classifiers.ensemble.boosting;
  * #L%
  */
 
+import org.apache.commons.collections.ArrayStack;
 import org.apache.samoa.core.ContentEvent;
 import org.apache.samoa.instances.Instance;
 import org.apache.samoa.learners.ResultContentEvent;
@@ -26,6 +27,12 @@ import org.apache.samoa.moa.core.DoubleVector;
 import org.apache.samoa.core.Processor;
 import org.apache.samoa.learners.InstanceContentEvent;
 import org.apache.samoa.topology.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Handles the updating of the model, and the propagation of the updated model at each super-step
@@ -38,6 +45,8 @@ import org.apache.samoa.topology.Stream;
  */
 public class BoostModelProcessor implements Processor {
 
+  private static final Logger logger =
+      LoggerFactory.getLogger(BoostModelProcessor.class);
   private static final long serialVersionUID = -5393272885824859630L;
   // The boosting model maintains the state of the boosting model we use
   private BoostingModel boostingModel;
@@ -47,11 +56,19 @@ public class BoostModelProcessor implements Processor {
   // and propagating them down the line (e.g. to the evaluator)
   private Stream outputStream;
   private int ensembleSize;
+  // Some debugging metrics
+  private long inputsSinceOutput = 0;
+  private long totalOutputs = 0;
+  private long totalInputs = 0;
+  // Used to track the index of the last instance we fully processed
   private long lastProcessedInstanceIndex = 0;
+  // Buffer to hold incoming events until all the preceding ones have been fully processed
+  private ArrayDeque<InstanceContentEvent> instanceBuffer;
 
   public BoostModelProcessor(BoostingModel boostingModel, int ensembleSize) {
     this.boostingModel = boostingModel;
     this.ensembleSize = ensembleSize;
+    this.instanceBuffer = new ArrayDeque<>();
   }
 
   // Copy constructor. Note that these are all shallow copies
@@ -60,6 +77,7 @@ public class BoostModelProcessor implements Processor {
     this.outputStream = oldProcessor.outputStream;
     this.learnerStream = oldProcessor.learnerStream;
     this.ensembleSize = oldProcessor.ensembleSize;
+    this.instanceBuffer = oldProcessor.instanceBuffer;
   }
 
   /**
@@ -70,31 +88,59 @@ public class BoostModelProcessor implements Processor {
   public boolean process(ContentEvent event) {
 
     if (event instanceof InstanceContentEvent) { // Receive an instance event from the source
+      inputsSinceOutput++;
+      totalInputs++;
       // We augment the source event with the most up-to-date version of the model we have, and push it into the
       // boosting pipeline
       InstanceContentEvent inEvent = (InstanceContentEvent) event;
-      if (inEvent.getInstanceIndex() < 0) {
-        // end learning
-        int numClasses = inEvent.getInstanceContent().getInstance().numClasses();
-        learnerStream.put(new BoostContentEvent(
-            (InstanceContentEvent) event, boostingModel, new DoubleVector(new double[numClasses])));
-        return true;
-      }
-      if (inEvent.getInstanceIndex() !=  lastProcessedInstanceIndex + 1) {
-        // Reject the event until the previous instance has returned from the boosting pipeline
-        return false;
-      } else {
-        int numClasses = inEvent.getInstanceContent().getInstance().numClasses();
+      int numClasses = inEvent.getInstanceContent().getInstance().numClasses();
+
+      if (inEvent.getInstanceIndex() < 0) { // end learning
+        // Check if we still have events waiting to be processed, and flush them
+        if (!instanceBuffer.isEmpty()) {
+          logger.error("Number of events left over at end: " + instanceBuffer.size());
+          // Flush the remaining events in the stream and pray for the best :P
+          for (InstanceContentEvent leftoverEvent : instanceBuffer) {
+            learnerStream.put(new BoostContentEvent(
+                leftoverEvent, boostingModel, new DoubleVector(new double[numClasses])));
+          }
+        }
+        logger.info("Total inputs at end: " + totalInputs);
+
         learnerStream.put(new BoostContentEvent(
             (InstanceContentEvent) event, boostingModel, new DoubleVector(new double[numClasses])));
         return true;
       }
 
+      // If the incoming event is further ahead from the last instance we fully processed
+      if (inEvent.getInstanceIndex() >  lastProcessedInstanceIndex + 1) {
+        // Put the event in a buffer until all the previous events are processed
+        instanceBuffer.add(inEvent);
+
+        // Check if it's now possible to process the oldest event in the buffer
+        if (instanceBuffer.peek().getInstanceIndex() == lastProcessedInstanceIndex + 1) {
+          // If we've processed everything until the head of the queue, pop it and send it on.
+          learnerStream.put(new BoostContentEvent(
+              instanceBuffer.pop(), boostingModel, new DoubleVector(new double[numClasses])));
+          }
+      } else  { // Else the current event can be processed immediately
+        learnerStream.put(new BoostContentEvent(
+            (InstanceContentEvent) event, boostingModel, new DoubleVector(new double[numClasses])));
+      }
+      return true;
     } else { // Then we must have an BoostContentEvent
+      totalOutputs++;
+      if (totalInputs % 1000 == 0) {
+        logger.debug("Inputs since last output: " + inputsSinceOutput);
+        logger.debug("Total inputs: " + totalInputs);
+        logger.debug("Total outputs: " + totalOutputs);
+        inputsSinceOutput = 0;
+      }
       BoostContentEvent boostContentEvent = (BoostContentEvent) event;
       InstanceContentEvent inEvent = boostContentEvent.getInstanceContentEvent();
       Instance instance = inEvent.getInstance();
       if (inEvent.getInstanceIndex() < 0) {
+
         // end learning
         ResultContentEvent outContentEvent = new ResultContentEvent(-1, instance, 0,
             new double[0], true);
@@ -112,7 +158,6 @@ public class BoostModelProcessor implements Processor {
       ResultContentEvent outContentEvent = new ResultContentEvent(inEvent.getInstanceIndex(),
           instance, inEvent.getClassId(), predictionsSum.getArrayRef(), inEvent.isLastEvent());
       outputStream.put(outContentEvent);
-      // TODO: Handle last event?
       return true;
     }
   }
